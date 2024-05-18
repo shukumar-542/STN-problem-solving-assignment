2ef007421538446f4fa6700952d3d25a582011c3
shukumar-542 <shukumar542@gmail.com>
2024-05-18 16:03:54 +0600

    Solved find and modify problem

---
+++ findAndModify.js
@@ -0,0 +1,33 @@
+const peoples = [
+   
+    {
+        name: "John Smith",
+        age: 34,
+        gender: "Male"
+    },
+    {
+        name: "Chris Lee",
+        age: 25,
+        gender: "Female"
+    },
+    {
+        name: "David Warner",
+        age: 45,
+        gender: "Male"
+    },
+    {
+        name: "Eve Garcia",
+        age: 30,
+        gender: "Female"
+    }
+]
+
+const FindAndUpdateAge = (name, newAge)=>{
+    const findPeople = peoples.find(person => person.name === name);
+    if(findPeople){
+        findPeople.age = newAge
+    }
+    
+}
+ FindAndUpdateAge('Eve Garcia' , 50);
+console.log(peoples);
