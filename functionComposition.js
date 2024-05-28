e2a493dc23324ccff9743357f0d4977c0d62d3e5
shukumar-542 <shukumar542@gmail.com>
2024-05-29 01:44:23 +0600

    Solved composite function problem

--- functionComposition.js
+++ functionComposition.js
@@ -21,17 +21,31 @@
 
 // function composition is the 
 
-function print(result){
-    console.log(result);
+// function print(result){
+//     console.log(result);
+// }
+
+// function add (a, b){
+//     return a + b
+// }
+
+// function multiply(num){
+//     return num * 2
+// }
+
+//  print(multiply(add(3,5)))
+
+// Write Three Functions: One To Square A Number, One To Double A Number, And One To Add 5 To A Number. Compose These Functions To Create A New Function That Squares A Number, Doubles The Result, And Then Adds 5.
+
+const square = (number)=>{
+    return number * number
+}
+const double = (number)=>{
+    return number * 2
+}
+const add5 = (number) =>{
+    return number + 5
 }
 
-function add (a, b){
-    return a + b
-}
-
-function multiply(num){
-    return num * 2
-}
-
- print(multiply(add(3,5)))
-
+const result  = add5(double(square(5)))
+console.log(result);
