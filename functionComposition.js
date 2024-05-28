20b418881fcc908efb15a12b4d2d66ea9fd9b930
shukumar-542 <shukumar542@gmail.com>
2024-05-28 20:09:36 +0600

    Composition function

--- functionComposition.js
+++ functionComposition.js
@@ -1,19 +1,37 @@
-const numberSquare = (number)=>{
-    return number * number
+// const numberSquare = (number)=>{
+//     return number * number
+// }
+
+// const doubleNumber = (number)=>{
+//     return number * 2
+// }
+// const addFiveNumber = (number)=>{
+//     return number + 5
+// }
+
+// const compositionFunction =(number)=>{
+//     const square = numberSquare(number);
+//     const double = doubleNumber(square);
+//     return addFiveNumber(double)
+
+// }
+// const result = compositionFunction(5);
+// console.log(result);
+
+
+// function composition is the 
+
+function print(result){
+    console.log(result);
 }
 
-const doubleNumber = (number)=>{
-    return number * 2
-}
-const addFiveNumber = (number)=>{
-    return number + 5
+function add (a, b){
+    return a + b
 }
 
-const compositionFunction =(number)=>{
-    const square = numberSquare(number);
-    const double = doubleNumber(square);
-    return addFiveNumber(double)
-
+function multiply(num){
+    return num * 2
 }
-const result = compositionFunction(5);
-console.log(result);
+
+ print(multiply(add(3,5)))
+
